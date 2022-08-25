eaff26584ce7b1af4d978f5ffd1d78fb7add320e
bbbyqq <2287532812@qq.com>
2022-08-25 09:48:45 +0800

    new

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# bing_add_search
+# 图片预览：
+
+![image](https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/ac3fac9e90ecc0dfb54c3a110311daf157de1bded1108b29e9ddd9c5bfa92a54fefbc53063190e2d8350bc0108bd2c09?pictype=scale&from=30013&version=3.3.3.3&fname=Snipaste_2022-08-25_09-41-48.png&size=750)
+
+![image](https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/9094efb5c3a63f854c86e59f2d9b109b444902ddbb452e444dd3d1e6f70771ebd09a4f757293a1c164e7650fdda96bf1?pictype=scale&from=30013&version=3.3.3.3&fname=Snipaste_2022-08-25_09-42-12.png&size=750)
---
+++ tampermonkey.js
@@ -0,0 +1,121 @@
+// ==UserScript==
+// @name         必应新增常见搜索
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  必应新增常见搜索
+// @author       bbbyqq
+// @match        *://www.cn.bing.com/*
+// @match        *://cn.bing.com/*
+// @match        *://www.bing.com/*
+// @match        *://bing.com/*
+// @grant        GM_addStyle
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+  let div = `
+    <div id="btn_list">
+        <button class="add_btn" id="baidu_btn">百度</button>
+        <button class="add_btn" id="segmentfault_btn">segmentfault</button>
+        <button class="add_btn" id="google_btn">谷歌</button>
+        <button class="add_btn" id="google_translate_btn">谷歌翻译</button>
+        <button class="add_btn" id="zhihu_btn">知乎</button>
+        <button class="add_btn" id="toutiao_btn">头条</button>
+        <button class="add_btn" id="douban_btn">豆瓣</button>
+        <button class="add_btn" id="douyin_btn">抖音</button>
+    </div>
+    `
+
+  if ($('.b_scopebar').length) { // 搜索页
+    $('#est_switch').after(div)
+    $('#btn_list').attr("class", "btn_search")
+    readyClick()
+  } else { // 初始页
+    setTimeout(() => {
+      $('#est_switch').before(div)
+      $('#btn_list').attr("class", "btn_home")
+      readyClick()
+    }, 500)
+  }
+
+  function readyClick() {
+    const inputVal = $('#sb_form_q')
+
+    $("#baidu_btn").click(function () {
+      window.open(`https://www.baidu.com/s?wd=${inputVal.val()}`, '_blank')
+    })
+
+    $("#segmentfault_btn").click(function () {
+      window.open(`https://cn.bing.com/search?q=site:segmentfault.com%20${inputVal.val()}`, '_blank')
+    })
+
+    $('#google_btn').click(function () {
+      window.open(`https://www.google.com/search?q=${inputVal.val()}`, '_blank')
+    })
+
+    $('#google_translate_btn').click(function () {
+      window.open(`https://translate.google.cn/?sl=auto&tl=en&text=${inputVal.val()}&op=translate`, '_blank')
+    })
+
+    $("#zhihu_btn").click(function () {
+      window.open(`https://www.zhihu.com/search?type=content&q=${inputVal.val()}`, '_blank')
+    })
+
+    $("#toutiao_btn").click(function () {
+      window.open(`https://so.toutiao.com/search?dvpf=pc&source=input&keyword=${inputVal.val()}`, '_blank')
+    })
+
+    $("#douban_btn").click(function () {
+      window.open(`https://www.douban.com/search?source=suggest&q=${inputVal.val()}`, '_blank')
+    })
+
+    $("#douyin_btn").click(function () {
+      window.open(`https://www.douyin.com/search/${inputVal.val()}`, '_blank')
+    })
+  }
+
+  const css = `
+  .btn_search {
+    margin: 0 0 5px 160px;
+  }
+  
+  .btn_home {
+    margin-bottom: 5px;
+  }
+  
+  .add_btn {
+    color: #fff;
+    background-color: #1890ff;
+    border-color: #1890ff;
+    text-shadow: 0 -1px 0 rgb(0 0 0 / 12%);
+    line-height: 100%;
+    position: relative;
+    display: inline-block;
+    font-weight: 400;
+    white-space: nowrap;
+    text-align: center;
+    background-image: none;
+    box-shadow: 0 2px 0 rgb(0 0 0 / 2%);
+    cursor: pointer;
+    transition: all .3s cubic-bezier(.645,.045,.355,1);
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    touch-action: manipulation;
+    height: 100%;
+    padding: 5px 20px;
+    font-size: 16px;
+    border-radius: 30px;
+    border: none;
+    margin-right: 5px;
+  }
+  
+  .add_btn:hover {
+    color: #fff;
+    background-color: #40a9ff;
+  }`
+  GM_addStyle(css)
+
+})()
